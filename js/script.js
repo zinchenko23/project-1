45df21819556fc997900083f996e97a51722f501
Roman <zinchenko5838@gmail.com>
2021-11-18 13:31:35 +0200

    lesson016

--- js/script.js
+++ js/script.js
@@ -78,53 +78,87 @@
 // }
 //........1.3
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
-};
+// const personalMovieDB = {
+// 	count: numberOfFilms,
+// 	movies: {},
+// 	actors: {},
+// 	genres: [],
+// 	privat: false
+// };
 
 
-for (let i = 0; i < 2; i++) {
-	const a = prompt('Один из последних просмотренных фильмов?', ''),
-		b = prompt('На сколько оцените его?');
+// for (let i = 0; i < 2; i++) {
+// 	const a = prompt('Один из последних просмотренных фильмов?', ''),
+// 		b = prompt('На сколько оцените его?');
 	
-	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-		personalMovieDB.movies[a] = b;
-		console.log('done');
-	} else {
-		console.log('error');
-		i--;
-	}
+// 	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+// 		personalMovieDB.movies[a] = b;
+// 		console.log('done');
+// 	} else {
+// 		console.log('error');
+// 		i--;
+// 	}
 	
 	
-}
+// }
 
-if (personalMovieDB.count < 10) {
-	console.log("Просмотрено мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-	console.log("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-	console.log("Вы киноман");
-} else {
-	console.log("Произошла лшибка");
-}
+// if (personalMovieDB.count < 10) {
+// 	console.log("Просмотрено мало фильмов");
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+// 	console.log("Вы классический зритель");
+// } else if (personalMovieDB.count >= 30) {
+// 	console.log("Вы киноман");
+// } else {
+// 	console.log("Произошла лшибка");
+// }
 
-console.log(personalMovieDB);
-
-
+// console.log(personalMovieDB);
 
 
+// let num = 20;
+
+// function showFirstMessage(text) {
+// 	console.log(text);
+// 	let num = 10;
+// 	console.log(num)
+// }
+
+// showFirstMessage("Hello world!");
+// console.log(num);
+
+// function calc(a, b) {
+// 	return (a + b);
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
 
 
 
+// function ret() {
+// 	let num = 50;
+
+	// 
+// 	return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
 
 
 
+// const logger = function () {
+// 	console.log("hello");
+// };
+
+// logger();
+
+
+
+// const calc = (a, b) => a + b;
 
 
 
